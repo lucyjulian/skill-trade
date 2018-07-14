3f6e7c123e74d34daea965bf894ec7ee87b95bba
Ben <bwisler95@gmail.com>
2018-07-14 11:49:26 -0400

    Gerber

---
+++ final-project/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from "./components/Navbar"
+import Login from "./components/Login"
+
+class App extends Component {
+  render() {
+    return (
+<div>
+        <Navbar/>
+        <Login/>
+</div>
+    );
+  }
+}
+
+export default App;
