bde420b981aba07e479973d952dcf874cc16f85f
Justin <jwhittington92@yahoo.com>
2018-07-18 21:25:40 -0400

    first skeleton

---
+++ src/components/pages/profile.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+	render() {
+		return (
+			<div>
+				<p>User Profile</p>
+
+				<form action="/action_page.php">
+					<input type="text" name="firstname" value="Mickey" />
+
+					<input type="text" name="lastname" value="Mouse" />
+
+					<input type="submit" value="Submit" />
+				</form>
+			</div>
+		);
+	}
+}
+
+export default Profile;
