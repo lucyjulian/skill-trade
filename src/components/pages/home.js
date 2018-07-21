21940bb193671fad5e6da5e4768bef24b87a49ab
chelsg7 <chelsgieck@gmail.com>
2018-07-20 23:11:18 -0400

    Revert "components 1"

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
-import "../css/bootstrap.css";
 
 class Home extends Component {
   
 // if (loggedIn) {
   render() {
     return (
-      <div className = "container text-center mx-auto my-3">
-        <h1>
-          HOME PAGE (not logged in)
-        </h1>
+      <div>
+          
+        <p>HOME PAGE (not logged in)</p>
       </div>
     );
   }
