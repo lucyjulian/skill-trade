0205a3cd256d8f7c795631af3993893743539db9
thorc <chelsgieck@gmail.com>
2018-07-21 16:45:49 -0400

    first

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 // import "../../App.css";
-import "../css/bootstrap.css"
+import "../css/bootstrap.css";
 
 
 class Header extends React.Component {
