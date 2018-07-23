6e1833175ddba2a761784125d9cf9dd3a8c74844
Lucy Julian <ljulian@alumni.unc.edu>
2018-07-23 12:48:22 -0400

    my branch merged with master and organization fixed

--- server/routes/api/listing.js
+++ server/routes/api/listing.js
@@ -1,18 +1,19 @@
-const router = require("express").Router();
-const listingController = require("../../controllers/listingcont");
+const express = require('express');
+const router = express.Router();
+const listingController = require('../database/controllers/listingController');
+
+//Route to /listings/:tag
+router.route("/:tag")
+    .get(listingController.getListingByTag);
+
+//route to create a new listing and link it to the person's profile
+router.route("/:userID")
+    .post(listingController.postNewListingByUser);
 
 
-// // Matches with "/api/books"
-// router.route("/")
-//   .get(listingController.findAll)
-//   .post(listingController.create);
-
-// // Matches with "/api/books/:id" 
-// router
-//   .route("/:id")
-//   .get(listingController.findById)
-//   .put(listingController.update)
-//   .delete(listingController.remove);
+//route to delete a listing
+router.route("/delete/:id")
+    .delete(listingController.deleteListingByID);
 
 
-module.exports = router; 
+module.exports = router;
--- src/components/pages/browse.js
+++ src/components/pages/browse.js
@@ -1,20 +1,13 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
 import Header from '../Header/header';
-=======
 import "../css/bootstrap.css";
->>>>>>> master
 
 class Browse extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
-      <div>  
-=======
       <div class = "container text-center mx-auto">
           
->>>>>>> master
         <p>Browse Lessons</p>
         <Header />
       </div>
