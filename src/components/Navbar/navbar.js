c64247a7ef3f875847a10f94384a72b7a965a2f1
thorc <chelsgieck@gmail.com>
2018-07-23 21:19:32 -0400

    small changes + removing bootstrap

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 // import "../../App.css";
 import axios from "axios";
-import "../css/bootstrap.css";
+// import "../css/bootstrap.css";
 
 
 class Navbar extends React.Component {
